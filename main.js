ed078c0a193714476d315e0d6269ebd90815811e
kowchergit config --global user.email kowcher99@gmail.com <37480985+kowcher99@users.noreply.github.com>
2021-10-18 00:03:45 +0600

    Revert "final"
    This reverts commit d0376f5eb2d5e11551adb712b07938809b550ced.

--- main.js
+++ main.js
@@ -10,13 +10,14 @@ const p1BtnElm = document.querySelector(".p1Btn")
 const p2BtnElm = document.querySelector(".p2Btn")
 const resetElm = document.querySelector(".reset")
 
-let winScore = 0
+let winScore = "Please enter a Score to start the game"
 let p1Score = 0
 let p2Score = 0
 
-winingScoreElm.textContent = "Please enter a score to win"
-
-turn = ''
+winingScoreElm.textContent = winScore
+p1ScoreElm.textContent = p1Score
+p2ScoreElm.textContent = p2Score
+turn = ' player-1 || player-1'
 
 p1BtnElm.setAttribute('disabled', 'disabled')
 p2BtnElm.setAttribute('disabled', 'disabled')
@@ -24,7 +25,6 @@ p2BtnElm.setAttribute('disabled', 'disabled')
 formElm.addEventListener('submit', e => {
     const  inputValue = +inputElm.value
     e.preventDefault()
-
     if(inputValue === '' || inputValue < 1 ){
         if(!document.querySelector('.inval-input')){
             formElm.insertAdjacentHTML('beforebegin','<p class = "inval-input"> Please enter valid number</p>')
@@ -34,7 +34,7 @@ formElm.addEventListener('submit', e => {
             document.querySelector('.inval-input').remove()
         }
     winScore = +inputElm.value
-    winingScoreElm.innerHTML =  ` Player who will reach  <b>${winScore}</b> will win the game`
+    winingScoreElm.textContent = winScore
     inputElm.value = ''
     initStage()
     }
@@ -43,41 +43,27 @@ formElm.addEventListener('submit', e => {
 })
 
 p1BtnElm.addEventListener('click', e => {
-     
+    if (turn) {
         p1Score = p1Score + genRanNum(5)
         p1ScoreElm.textContent = p1Score
         turn = 'player-2'
         p1BtnElm.setAttribute('disabled', 'disabled')
         p2BtnElm.removeAttribute('disabled')
-    
+    }
     checkWinner()
 
 })
 
 //checking who is winner
-
 function checkWinner() {
-    const p1Winscore = winScore <= p1Score
-    const p2Winscore = winScore <= p2Score
+    const p1Winscore = winScore === p1Score
+    const p2Winscore = winScore === p2Score
     if (p1Winscore || p2Winscore) {
         p1BtnElm.setAttribute('disabled', 'disabled')
         p2BtnElm.setAttribute('disabled', 'disabled')
     }
     displayWinner(p1Winscore, p2Winscore)
 }
-
-// function checkWinner() {
-//     if (p1Score >= winScore) {
-//         formElm.insertAdjacentHTML("beforebegin", "<h4><p class = 'winMsg'>Player 1 is the winner!</p></h4>");
-//         p1BtnElm.setAttribute("disabled", "disabled");
-//         p2BtnElm.setAttribute("disabled", "disabled");
-//     } else if (p2Score >= winScore) {
-//         formElm.insertAdjacentHTML("beforebegin", "<h4><p class = 'winMsg'>Player 2 is the winner!</p></h4>");
-//         p1BtnElm.setAttribute("disabled", "disabled");
-//         p2BtnElm.setAttribute("disabled", "disabled");
-//     }
-// }
-
 
 function displayWinner(p1, p2) {
     if (p1) {
@@ -89,19 +75,18 @@ function displayWinner(p1, p2) {
 
 
 p2BtnElm.addEventListener('click', e => {
-
+    if (turn) {
         p2Score = p2Score + genRanNum(5)
         p2ScoreElm.textContent = p2Score
         turn = 'player-1'
         p2BtnElm.setAttribute('disabled', 'disabled')
         p1BtnElm.removeAttribute('disabled')
-    
+    }
     checkWinner()
 })
 
 resetElm.addEventListener('click', e => {
-    winingScoreElm.textContent = "Please enter a score to win"
-    winScore = 0
+    winScore = "Please enter a Score to start the game"
     initStage()
 })
 
@@ -109,10 +94,12 @@ function initStage(){
     p1Score = 0
     p2Score = 0
     turn = 'player-1'
+    winingScoreElm.textContent = winScore
     p1ScoreElm.textContent = p1Score
     p2ScoreElm.textContent = p2Score
     p1BtnElm.setAttribute('disabled', 'disabled')
     p2BtnElm.setAttribute('disabled', 'disabled')
+
     if(document.querySelector('.winMsg')){
         document.querySelector('.winMsg').remove()
     }
